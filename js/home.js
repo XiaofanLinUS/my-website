3cf751e12fe12137f7cb79d1afc491cea0d1cf75
xiaofanlinus <xiaofanlinus@gmail.com>
2016-10-18 17:06:51 -0700

    Succeed in rotating the background image of heading with a fading effect

--- js/home.js
+++ js/home.js
@@ -1,12 +1,13 @@
 var $header = $('header');
 var imgSrc = ['./img/heading.jpg' , './img/e.jpg', './img/s.jpg'];
 var index = 0;
-var interval = 2500;
+var interval = 1500;
 var bp = $header.css("background-image");
 setInterval(function() {
-    bp = bp.substring(bp.indexOf(",")+1);
+    bp = bp.substring(bp.indexOf(")")+1);
     index = (index + 1) % imgSrc.length;
-    var backProp = bp  +  ", url('"
+    var backProp = bp  +  " url('"
 	   + imgSrc[index] + "')";
     $header.css("background-image", backProp);
+    console.log(backProp);
 }, interval);
